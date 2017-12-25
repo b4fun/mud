c686a33159eb4119cfde8f5331f846e9767c7dad
hbc <bcxxxxxx@gmail.com>
2017-12-25 13:23:10 +0800

    init app

---
+++ app/webpack.config.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// Make sure any symlinks in the project folder are resolved:
+// https://github.com/facebookincubator/create-react-app/issues/637
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+
+const paths = {
+  appBuild: resolveApp('build'),
+  appNodeModules: resolveApp('node_modules'),
+};
+
+module.exports = {
+  entry: {
+    bundle: [resolveApp('src/main')],
+  },
+  output: {
+    path: paths.appBuild,
+    filename: 'static/js/[name].[chunkhash:8].js',
+    chunkFilename: 'static/js/[name].[chunkhash:8].chunk.js',
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.tsx?$/,
+        loaders: ['ts-loader'],
+        exclude: [/node_modules/, paths.appNodeModules],
+      },
+    ]
+  },
+  externals: {
+    // don't bundle the 'react' npm package with our bundle.js
+    // but get it from a global 'React' variable
+    // 'react': 'React'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['.ts', '.js'],
+
+    // this is only required when we "import 'jquery'"
+    // 'alias': { 'jquery': path.join(__dirname, "vendor", "jquery-2.2.0.min.js") }
+  }
+};
