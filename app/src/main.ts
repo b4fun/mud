ad47d782b6e72adcc1175f28e6a833d0d23a25b2
hbc <bcxxxxxx@gmail.com>
2018-01-06 14:15:27 +0800

    feat: implement hexagon map render

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,4 +1,27 @@
 import * as PIXI from 'pixi.js'
 
-const app = new PIXI.Application()
+import renderHexagonMap from './pixi/hexagon_render'
+
+const MAP_SIZE = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+}
+const app = new PIXI.Application({
+    width: MAP_SIZE.width,
+    height: MAP_SIZE.height,
+})
 document.body.appendChild(app.view)
+
+PIXI.loader
+    .add('tile_sand', 'hexagonPack/PNG/Tiles/Terrain/Sand/sand_02.png')
+    .load((loader, resources) => {
+        const sandTexture: PIXI.Texture = resources.tile_sand.texture
+
+        renderHexagonMap(
+            app.stage,
+            sandTexture,
+            MAP_SIZE.width,
+            MAP_SIZE.height,
+            .7,
+        )
+    })
